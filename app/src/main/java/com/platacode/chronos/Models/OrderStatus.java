7cf809ed10860563fa0fe028c577ddb4d9e900f1
John Aldrin Plata <king.aldrin31@gmail.com>
2018-01-24 19:09:50 +0800

    removes timeline-view and its implementation

--- app/src/main/java/com/platacode/chronos/Models/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.platacode.chronos.Models;
-
-public enum OrderStatus {
-    COMPLETED,
-    ACTIVE,
-    INACTIVE
-}
--- app/src/main/java/com/platacode/chronos/Models/Orientation.java
+++
@@ -1,7 +0,0 @@
-package com.platacode.chronos.Models;
-
-
-public enum Orientation {
-    VERTICAL,
-    HORIZONTAL
-}
